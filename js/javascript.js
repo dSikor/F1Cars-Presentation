d1aa64cad931e1471096973a0e158df2b9592337
dSikor <dsikora08@gmail.com>
2021-06-05 15:45:52 +0200

    change: use promises to sequence functions

--- js/javascript.js
+++ js/javascript.js
@@ -1,37 +1,39 @@
-
-
-let driveElementOut=(callback)=>{
-
-    console.log("ANIMATE");
-    let myElement1 = document.getElementsByClassName('car1_image');
-    for(i = 0; i < myElement1.length; i++) {
-        myElement1[i].style.animationName="driveCar1Out";
-    }
-    let myElement2 = document.getElementsByClassName('car2_image');
-    for(i = 0; i < myElement2.length; i++) {
-        myElement2[i].style.animationName="driveCar2Out";
-    }
-
-    callback();
+let containerChange=()=>{
+    sequence();
 }
+
+let driveElementOut=()=>{
+
+    return new Promise((resolve)=>{
+        this.setTimeout(()=> {
+
+            let myElement1 = document.getElementsByClassName('car1_animate');
+            for(i = 0; i < myElement1.length; i++) {
+                myElement1[i].style.animationName="driveCar1Out";
+            }
+            let myElement2 = document.getElementsByClassName('car2_animate');
+            for(i = 0; i < myElement2.length; i++) {
+                myElement2[i].style.animationName="driveCar2Out";
+            }
+
+          resolve()
+        }, 100)
+      })
+
+}
+
 let deleteElementCarsAndButton=()=>{
-    
-    console.log("DELETE");
-    let imageElements = document.getElementsByTagName("img");
+
+     let imageElements = document.getElementsByClassName("car");
     Array.from(imageElements).forEach(element => {
         element.parentNode.removeChild(element);
     });
-    console.log(imageElements);
-    
+    imageElements = document.getElementsByClassName("car");
 }
 
-let containerChange=()=>{
-    driveElementOut(deleteElementCarsAndButton);
-    // deleteElementCarsAndButton();
-
+let sequence=()=> {
+    driveElementOut().then(() => {
+      return deleteElementCarsAndButton()
+    });
+  
 }
-
-
-
-
-
